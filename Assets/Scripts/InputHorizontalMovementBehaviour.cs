a8d1fa4609124d91044dc3b047635221f602911d
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-08-16 18:40:56 -0300

    Add limit horizontal

--- Assets/Scripts/InputHorizontalMovementBehaviour.cs
+++ Assets/Scripts/InputHorizontalMovementBehaviour.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class InputHorizontalMovementBehaviour : MonoBehaviour
 {
     public float speed = 0.3f;
+    public float limit = 6.7f;
 
     // Update is called once per frame
     void Update()
     {
         Vector2 currentPosition = transform.position;
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow)) {
             currentPosition.x += speed;
-        else if (Input.GetKey(KeyCode.LeftArrow))
+            if (currentPosition.x > limit)
+                currentPosition.x = limit;
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
             currentPosition.x -= speed;
-
+            if (currentPosition.x < (-1 * limit))
+                currentPosition.x = (-1 * limit);
+        }
+            
         transform.position = currentPosition;
     }
 }
