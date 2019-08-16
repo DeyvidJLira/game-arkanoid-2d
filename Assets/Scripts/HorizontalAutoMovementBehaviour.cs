8314782de5a99eaeee3e05afa1a653a02545457f
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-08-16 12:03:13 -0300

    Sliced paddle and background image

--- Assets/Scripts/HorizontalAutoMovementBehaviour.cs
+++ Assets/Scripts/HorizontalAutoMovementBehaviour.cs
@@ -2,22 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HorizontalAutoMovementBehaviour : MonoBehaviour
-{   
+public class HorizontalAutoMovementBehaviour : MonoBehaviour {  
     public int distance;
     public float speed = 0.2f;
     int direction = 1;
     Vector2 startPosition;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         startPosition = transform.position;   
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if(Mathf.Abs(transform.position.x - startPosition.x) > distance)
             direction *= -1;
         transform.Translate(speed * direction, 0, 0);   
