a1b32f91f8df1cc2a32489cdc363e9e88818b2cd
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-08-16 23:51:10 -0300

    Create Brick / Create BrickColorBehaviour script

---
+++ Assets/Scripts/BrickColorBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickColorBehaviour : MonoBehaviour
+{
+    public Color[] damageColor;
+    int maxHits;
+    protected int hitNumbers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitNumbers = 0;
+        maxHits = damageColor.Length;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        GetComponent<SpriteRenderer>().color = damageColor[hitNumbers];
+
+        hitNumbers++;
+
+        if (hitNumbers >= maxHits)
+            Destroy(gameObject);
+    }
+}
