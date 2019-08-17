63141bf4243a02337c04e0e82245c18b795706cc
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-08-16 21:56:10 -0300

    Change gravity scale of Ball / Create ArkanoidBallBehaviour script

---
+++ Assets/Scripts/ArkanoidBallBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArkanoidBallBehaviour : MonoBehaviour
+{
+    public float startSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = (Vector2.up + Vector2.right) * startSpeed; 
+    }
+
+}
