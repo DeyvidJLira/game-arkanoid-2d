4a81d5b9edca42ab2abcc3d6a3bbff1be4050d30
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-08-16 21:44:43 -0300

    Add rigidbody2d for Paddle / Create InputHorizontalPhysicMovmentBehaviour script

---
+++ Assets/Scripts/InputHorizontalPhysicMovementBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHorizontalPhysicMovementBehaviour : MonoBehaviour
+{
+    public float speed = 0.3f;
+    public float limit = 6.7f;
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 currentPosition = transform.position;
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            currentPosition.x += speed;
+            if (currentPosition.x > limit)
+                currentPosition.x = limit;
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+            currentPosition.x -= speed;
+            if (currentPosition.x < (-1 * limit))
+                currentPosition.x = (-1 * limit);
+        }
+
+        GetComponent<Rigidbody2D>().MovePosition(currentPosition);
+    }
+}
