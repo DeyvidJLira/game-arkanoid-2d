3f9c8b75c324739df9f331f871ba405c7ca96433
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-08-19 23:24:16 -0300

    Create BrickBreak with variant red / Create BrickBreakBehaviour and BallCollisionSoundBehaviour scripts / Implement sounds

---
+++ Assets/Scripts/BallCollisionSoundBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollisionSoundBehaviour : MonoBehaviour
+{
+    public AudioSource soundFXBrickWall;
+    public AudioSource soundFXBrickHit;
+    public AudioSource soundFXBrickBroken;
+    public AudioSource soundTrack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundTrack.Play();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.tag == "Wall" || collision.collider.tag == "Paddle")
+            soundFXBrickWall.Play();
+        else if (collision.collider.tag == "Brick")
+            soundFXBrickHit.Play();
+    }
+}
