c571923efc32c5218a1ecd7aee36980dba13c83f
Deyvid Jaguaribe de Lira <deyvidjlira@gmail.com>
2019-08-25 12:32:58 -0300

    Create Credits scene / Remove files unnecessaries

--- Assets/Scripts/StartMenuBehaviour.cs
+++ Assets/Scripts/StartMenuBehaviour.cs
@@ -5,11 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class StartMenuBehaviour : MonoBehaviour
 {
+
+    public GameObject startPanel;
+    public GameObject creditsPanel;
+
     public void playGame() {
         SceneManager.LoadScene("Scenes/MainGame");
     }
 
     public void credits() {
+        startPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
 
+    public void startMenu() {
+        creditsPanel.SetActive(false);
+        startPanel.SetActive(true);
     }
 }
